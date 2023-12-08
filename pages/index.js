76d348f453298989a94c33f745e5d443ad6bcac1
nenadmne <cosovicnenad14@gmail.com>
2023-12-08 14:07:33 +0100

    Adding Homepage components and pre build module css styles for it

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,13 @@
+import { Fragment } from "react";
+import Hero from "../components/home-page/hero";
+import FeaturedPosts from "../components/home-page/featured-posts";
+
 export default function HomePage() {
-  return <h1> Homepage </h1>;
+  return (
+    <Fragment>
+      <h1> Homepage </h1>
+      <Hero />
+      <FeaturedPosts />
+    </Fragment>
+  );
 }
