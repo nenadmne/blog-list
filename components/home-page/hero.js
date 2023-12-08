9de24d70bb3bd8e55e85f39c4a3626ac94639f0b
nenadmne <cosovicnenad14@gmail.com>
2023-12-08 14:35:02 +0100

    Adding some dummy data to hero componenent

--- components/home-page/hero.js
+++ components/home-page/hero.js
@@ -1,3 +1,19 @@
+import Image from "next/image";
+import classes from "./hero.module.css";
+
 export default function Hero() {
-  return <h1> Hero </h1>;
+  return (
+    <section className={classes.hero}>
+      <div className={classes.image}>
+        <Image
+          src="/images/site/max.png"
+          alt="An image showing profile"
+          width={300}
+          height={300}
+        />
+      </div>
+      <h1> Hi, I'M Max </h1>
+      <p> About bloger interests and posts</p>
+    </section>
+  );
 }
