290181c30350ef40d7266f6587827c9dbb63b24e
nenadmne <cosovicnenad14@gmail.com>
2023-12-09 20:06:54 +0100

    Adding post-item componeent structure and styles

--- components/posts/post-item.js
+++ components/posts/post-item.js
@@ -1,9 +1,30 @@
-import classes from "./posts-grid.module.css";
+import Link from "next/link";
+import classes from "./post-item.module.css";
+import Image from "next/image";
 
-export default function PostItem() {
+export default function PostItem(props) {
+  const { title, image, excerpt, date, slug } = props.post;
+
+  const formattedDate = new Date(date).toLocaleDateString("en-Us", {
+    day: "numeric",
+    month: "long",
+    year: "numeric",
+  });
+
+  const imagePath = `/images/posts/${slug}/${image}`;
+
   return (
-    <section>
-      <h2> Posts Grid </h2>
-    </section>
+    <li className={classes.post}>
+      <Link>
+        <div className={classes.image}>
+          <Image src={imagePath} alt={title} width={300} height={200} />
+        </div>
+        <div className={classes.content}>
+          <h3> {title} </h3>
+          <time> {formattedDate} </time>
+          <p> {excerpt} </p>
+        </div>
+      </Link>
+    </li>
   );
 }
