7167c1817f449cd1e392a2b34218440d4b03cb11
nenadmne <cosovicnenad14@gmail.com>
2023-12-10 15:24:31 +0100

    Added all posts component structure and some dummy data

--- pages/blogposts/index.js
+++ pages/blogposts/index.js
@@ -1,3 +1,37 @@
+import { Fragment } from "react";
+import AllPosts from "../../components/posts/all-posts";
+
+const DUMMY_POSTS = [
+  {
+    slug: "getting-started-with-nextjs",
+    title: "Getting started with next JS",
+    excerpt:
+      "Next Js is React framework for prodaction. It makes fullstack React apps easier and better",
+    image: "getting-started-nextjs.png",
+    date: 2023 - 12 - 12,
+  },
+  {
+    slug: "getting-started-with-nextjs",
+    title: "Getting started with next JS",
+    excerpt:
+      "Next Js is React framework for prodaction. It makes fullstack React apps easier and better",
+    image: "getting-started-nextjs.png",
+    date: 2023 - 12 - 12,
+  },
+  {
+    slug: "getting-started-with-nextjs",
+    title: "Getting started with next JS",
+    excerpt:
+      "Next Js is React framework for prodaction. It makes fullstack React apps easier and better",
+    image: "getting-started-nextjs.png",
+    date: 2023 - 12 - 12,
+  },
+];
+
 export default function BlogPosts() {
-  return <h1> BlogPosts </h1>;
+  return (
+    <Fragment>
+      <AllPosts posts={DUMMY_POSTS} />
+    </Fragment>
+  );
 }
