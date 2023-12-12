8e25d27dcadf73521eb6f317a26a43cce1f83efd
nenadmne <cosovicnenad14@gmail.com>
2023-12-12 22:53:59 +0100

    Added get static props on featured projects

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,7 @@
 import { Fragment } from "react";
 import Hero from "../components/home-page/hero";
 import FeaturedPosts from "../components/home-page/featured-posts";
+import getFeaturedPosts from "../lib/posts-util";
 
 const DUMMY_POSTS = [
   {
@@ -29,11 +30,21 @@ const DUMMY_POSTS = [
   },
 ];
 
-export default function HomePage() {
+export default function HomePage({ posts }) {
   return (
     <Fragment>
       <Hero />
-      <FeaturedPosts posts={DUMMY_POSTS} />
+      <FeaturedPosts posts={posts} />
     </Fragment>
   );
+}
+
+export function getStaticProps() {
+  const featuredPosts = getFeaturedPosts();
+
+  return {
+    props: {
+      posts: featuredPosts,
+    },
+  };
 }
--- posts/getting-started-with-nextjs.md
+++ posts/getting-started-with-nextjs.md
@@ -1,9 +1,9 @@
 ---
-slug: "getting-started-with-nextjs"
 title: "Getting started with next JS"
 excerpt: Next Js is React framework for prodaction. It makes fullstack React apps easier and better
-image: getting-started-nextjs.png,
-date: 2023 - 12 - 12,
+image: getting-started-nextjs.png
+date: 2023 - 12 - 12
+isFeatured: true
 ---
 
 # This is a title
