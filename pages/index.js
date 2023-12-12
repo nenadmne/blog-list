007819c7bbe42d373ba035bbff9b47cb56a5b4fb
nenadmne <cosovicnenad14@gmail.com>
2023-12-12 23:00:56 +0100

    Removed dummy posts from homepage

--- pages/index.js
+++ pages/index.js
@@ -3,33 +3,6 @@ import Hero from "../components/home-page/hero";
 import FeaturedPosts from "../components/home-page/featured-posts";
 import getFeaturedPosts from "../lib/posts-util";
 
-const DUMMY_POSTS = [
-  {
-    slug: "getting-started-with-nextjs",
-    title: "Getting started with next JS",
-    excerpt:
-      "Next Js is React framework for prodaction. It makes fullstack React apps easier and better",
-    image: "getting-started-nextjs.png",
-    date: 2023 - 12 - 12,
-  },
-  {
-    slug: "getting-started-with-nextjs",
-    title: "Getting started with next JS",
-    excerpt:
-      "Next Js is React framework for prodaction. It makes fullstack React apps easier and better",
-    image: "getting-started-nextjs.png",
-    date: 2023 - 12 - 12,
-  },
-  {
-    slug: "getting-started-with-nextjs",
-    title: "Getting started with next JS",
-    excerpt:
-      "Next Js is React framework for prodaction. It makes fullstack React apps easier and better",
-    image: "getting-started-nextjs.png",
-    date: 2023 - 12 - 12,
-  },
-];
-
 export default function HomePage({ posts }) {
   return (
     <Fragment>
