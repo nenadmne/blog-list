e1ec9edfdd7e3cd5bb175a6e27d0e3878a3f8f9e
nenadmne <cosovicnenad14@gmail.com>
2023-12-13 21:58:31 +0100

    Writting util function for geting single post file by slug

--- pages/blogposts/[slug].js
+++ pages/blogposts/[slug].js
@@ -1,5 +1,19 @@
 import PostContent from "../../components/posts/post-detail/post-content";
+import { getPostData } from "../../lib/posts-util";
 
 export default function BlogPost() {
   return <PostContent />;
+}
+
+export function getStaticProps(context) {
+  const { params } = context;
+  const { slug } = params;
+  const post = getPostData(slug);
+
+  return {
+    props: {
+      post: post,
+    },
+    revalidate: 600,
+  };
 }
