f4687c6779ab9bdc8705dbd477fcd5769c08190f
nenadmne <cosovicnenad14@gmail.com>
2023-12-17 21:52:40 +0100

    Connecting messages to mongo database

--- pages/api/contact.js
+++ pages/api/contact.js
@@ -1,4 +1,8 @@
-export default function Handler(req, res) {
+import { MongoClient } from "mongodb";
+
+export default async function Handler(req, res) {
+  MongoClient;
+
   if (req.method === "POST") {
     const { email, name, message } = req.body;
     if (
@@ -18,7 +22,30 @@ export default function Handler(req, res) {
       name,
       message,
     };
-    console.log(newMessage);
+
+    let client;
+    try {
+      client = await MongoClient.connect(
+        "mongodb+srv://cosovicnenad14:arsenal95@cluster0.0rzttmk.mongodb.net/blog-list?retryWrites=true&w=majority"
+      );
+    } catch (error) {
+      res.status(500).json({ message: "Could not connect to database" });
+      return;
+    }
+
+    const db = client.db();
+
+    try {
+      await db.collection("messages").insertOne(newMessage);
+    } catch (error) {
+      res
+        .status(500)
+        .json({ message: "Could not store messages into client collection" });
+      return;
+    }
+
+    client.close();
+
     res
       .status(200)
       .json({ message: "Successfully stored message", message: newMessage });
