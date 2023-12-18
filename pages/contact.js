f49313be372da4007629ae8fc629c19388eae8dd
nenadmne <cosovicnenad14@gmail.com>
2023-12-18 21:17:01 +0100

    Adding meta data to pages

--- pages/contact.js
+++ pages/contact.js
@@ -1,5 +1,15 @@
+import { Fragment } from "react";
+import Head from "next/head";
 import ContactForm from "../components/contact/contact-form";
 
 export default function Contact() {
-  return <ContactForm />;
+  return (
+    <Fragment>
+      <Head>
+        <title> Contact </title>
+        <meta name="description" content="Send me your messages" />
+      </Head>
+      <ContactForm />
+    </Fragment>
+  );
 }
